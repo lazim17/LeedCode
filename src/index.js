662127f40b9277378cb777481d9ab941d09354c1
Arjun Vijayan <vijayanarjun4@gmail.com>
2023-11-03 21:01:00 +0530

    Initial Routes Setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CodeEditor from './CodeEditor';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <CodeEditor />
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
